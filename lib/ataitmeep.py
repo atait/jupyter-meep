67d734095bd1aa0b0c91daef0248da2199f4fcd8
Alex Tait <alexander.tait@nist.gov>
2018-10-30 11:10:43 -0600

    cool stuff with notebook liveplotting

--- lib/ataitmeep.py
+++ lib/ataitmeep.py
@@ -13,3 +13,24 @@ def show_geometry(sim):
     plt.figure(dpi=100)
     plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
     return eps_data
+
+
+_field_artist = None
+def liveplot(sim):
+    ''' You must put ``mp.at_beginning(liveplot)`` in your arguments to run!
+        Make sure to turn the progress_interval up before using this
+    '''
+    if sim.meep_time() == 0.:
+        eps_data = sim.get_array(center=mp.Vector3(), size=sim.cell_size, component=mp.Dielectric)
+        dielectric_artist = plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+    # now do the field
+    ez_data = sim.get_array(center=mp.Vector3(), size=sim.cell_size, component=mp.Ez)
+    global _field_artist
+    if sim.meep_time() == 0.:
+        _field_artist = plt.imshow(ez_data.transpose(), interpolation='spline36', cmap='RdBu',
+                                   alpha=0.9, vmin=-.1, vmax=.1)
+    else:
+        _field_artist.set_data(ez_data.transpose())
+    plt.title(f't = {sim.meep_time()}')
+    display.display(plt.gcf())
+    display.clear_output(wait=True)
