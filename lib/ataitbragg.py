13e53dc884b06dea9b87279159adf98bdeafec22
Alex Tait <alexander.tait@nist.gov>
2018-11-26 07:03:22 -0700

    pulled a bunch of stuff into library

---
+++ lib/ataitbragg.py
@@ -0,0 +1,147 @@
+from ataitmeep import objview, silicon, oxide, liveplot
+import meep as mp
+
+is3D = False
+
+# geo is the parameters, while geometry is the MEEP geometry list
+default_geo = objview(
+                        sm_width = 0.35,  # um
+                        pitch = .28,
+                        duty = .5,
+                        dw = .2,
+                        n_periods = 10,
+                        buffer = 5,  # um
+                        thickness = 0.,  # change to 0 for 2D
+                       )
+
+
+def kwargs_to_geo(geo=None, **kwargs):
+    if geo is None:
+        geo = default_geo
+    this_geo = geo.copy()
+    this_geo.update(**kwargs)
+    return this_geo
+
+
+fcen = 1 / 1.22
+df = fcen / 5
+nfreq = 101
+def bragg_source(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    # sources = []
+    sources = [mp.Source(mp.GaussianSource(fcen, fwidth=df),
+                     component=mp.Ey,
+                     center=mp.Vector3(-cell_x(geo) / 2 + dpml, 0),
+                     size=mp.Vector3(0, geo.sm_width, geo.thickness))]
+    return sources
+
+
+dpml = 1.
+pml_layers = [mp.PML(dpml)]
+resolution = 20
+# this should be replaced by an eigenmode
+def sim_kwargs(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    symms = [mp.Mirror(mp.Y, phase=-1)]
+    if geo.thickness != 0:
+        symms.append(mp.Mirror(mp.Z, phase=1))
+    return dict(
+                  cell_size=bragg_cell(geo),
+                  geometry=bragg_geometry(geo),
+                  sources=bragg_source(geo),
+                  boundary_layers=pml_layers,
+                  resolution=resolution,
+                  default_material=oxide,
+                  symmetries=symms,
+                  # progress_interval=1e6,
+                  )
+
+
+def cell_x(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    return geo.pitch * geo.n_periods + 2 * geo.buffer
+
+
+def bragg_cell(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    cell_y = 3
+    cell_z = 0 if geo.thickness == 0 else geo.thickness + 3
+    cell = mp.Vector3(cell_x(geo), cell_y, cell_z)
+    return cell
+
+
+def bragg_geometry(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    cellx = cell_x(geo)
+
+    geometry = []
+    for side_sign in [-1, 1]:
+        geometry.append(mp.Block(mp.Vector3(geo.buffer, geo.sm_width, geo.thickness),
+                                 center=mp.Vector3((cellx - geo.buffer) * side_sign/2),
+                                 material=silicon))
+    # the teeth of the grating
+    x0 = geo.buffer - cellx/2
+    for iPeriod in range(geo.n_periods):
+        for iTooth in range(2):
+            tooth_len = geo.pitch * (geo.duty if iTooth == 0 else (1 - geo.duty))
+            wg_wid = geo.sm_width + geo.dw / 2 * (1 if iTooth == 0 else -1)
+            geometry.append(mp.Block(mp.Vector3(tooth_len, wg_wid, geo.thickness),
+                                     center=mp.Vector3(x0 + tooth_len/2),
+                                     material=silicon))
+            x0 += tooth_len
+    return geometry
+
+
+def monitor_x(geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    return cell_x(geo) / 2 - dpml - .5
+
+
+def add_monitors(simulation, geo=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+
+    # reflected flux
+    refl_fr = mp.FluxRegion(center=mp.Vector3(-monitor_x(geo) ,0, 0),
+                            size=mp.Vector3(0, 2*geo.sm_width, 2*geo.thickness))
+    refl = simulation.add_flux(fcen, df, nfreq, refl_fr)
+
+    # transmitted flux
+    tran_fr = mp.FluxRegion(center=mp.Vector3(monitor_x(geo) ,0, 0),
+                            size=mp.Vector3(0, 2*geo.sm_width, 2*geo.thickness))
+    tran = simulation.add_flux(fcen, df, nfreq, tran_fr)
+
+    return refl, tran
+
+
+def livefield(sim):
+    liveplot(sim, mp.Ey)
+
+
+def monitor_until(geo=None, until=None, **kwargs):
+    geo = kwargs_to_geo(geo, **kwargs)
+    stop_kwarg = dict()
+    if until is None:
+        monitor_point = mp.Vector3(monitor_x(geo), 0, 0)
+        stop_kwarg['until_after_sources'] = mp.stop_when_fields_decayed(20, mp.Ey, monitor_point, 1e-3)
+    else:
+        stop_kwarg['until'] = until
+    return stop_kwarg
+
+
+def do_simrun(base_refl_data=None, do_live=True, geo=None, **kwargs):
+    sim = mp.Simulation(
+                        progress_interval=1e6 if do_live else 4,
+                        **sim_kwargs(geo=geo, **kwargs))
+    sim.reset_meep()
+
+    # Now put in some flux monitors. Make sure the pulse source was selected
+    refl, tran = add_monitors(sim)
+
+    # for normal run, load negated fields to subtract incident from refl. fields
+    if base_refl_data is not None:
+        sim.load_minus_flux_data(refl, base_refl_data)
+
+    run_args = (mp.at_beginning(livefield), mp.at_every(5, livefield), ) if do_live else tuple()
+    sim.run(*run_args,
+            **monitor_until(geo=geo, **kwargs))
+    return sim, refl, tran
--- lib/ataitmeep.py
+++ lib/ataitmeep.py
@@ -8,16 +8,33 @@ from IPython import display
 import meep as mp
 from meep import mpb
 
+silicon = mp.Medium(epsilon=12)
+oxide = mp.Medium(epsilon=3.9)
+
+
 class objview(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    def update(self, *dicts, **kwargs):
+        for di in dicts:
+            self.__dict__.update(di)
+        self.__dict__.update(kwargs)
+
+    def copy(self):
+        new_obj = objview(**self.__dict__)
+        return new_obj
 
 
 def show_geometry(sim_or_solver, **mpb_kwargs):
     if isinstance(sim_or_solver, mp.Simulation):
         sim = sim_or_solver
         sim.run(until=.0)
+        # import pdb; pdb.set_trace()
         eps_data = sim.get_array(center=mp.Vector3(), size=sim.cell_size, component=mp.Dielectric)
+        if len(eps_data.shape) == 3:
+            eps_data = eps_data[:, :, int(eps_data.shape[2] / 2)]
+            # eps_data = eps_data[:, int(eps_data.shape[1] / 2), :]  # for x-z plane
     elif isinstance(sim_or_solver, mpb.ModeSolver):
         ms = sim_or_solver
         if not any(p in mpb_kwargs.keys() for p in ['periods', 'x', 'y', 'z']):
@@ -41,9 +58,13 @@ def liveplot(sim, component=mp.Ez):
     # component=mp.Ey
     if sim.meep_time() == 0.:
         eps_data = sim.get_array(center=mp.Vector3(), size=sim.cell_size, component=mp.Dielectric)
+        if len(eps_data.shape) == 3:
+            eps_data = eps_data[:, :, int(eps_data.shape[2] / 2)]
         dielectric_artist = plt.imshow(eps_data.transpose()[::-1], interpolation='spline36', cmap='binary')
     # now do the field
     field_data = sim.get_array(center=mp.Vector3(), size=sim.cell_size, component=component)
+    if len(field_data.shape) == 3:
+            field_data = field_data[:, :, int(field_data.shape[2] / 2)]
     global _field_artist
     if sim.meep_time() == 0.:
         _field_artist = plt.imshow(field_data.transpose()[::-1], interpolation='spline36', cmap='RdBu',
