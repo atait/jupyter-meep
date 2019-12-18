da1c20be8897b8dc2b2065dd114052d29f974372
Alex Tait <alexander.tait@nist.gov>
2019-12-17 17:17:46 -0700

    progress on eigenmodes of partial WG

--- notebooks/partial_waveguides/partial_wgs.py
+++ notebooks/partial_waveguides/partial_wgs.py
@@ -2,43 +2,54 @@ import meep as mp
 from meep import mpb
 import numpy as np
 Si = mp.Medium(index=3.45)
+ColdSi = mp.Medium(index=3.49)
 SiO2 = mp.Medium(index=1.45)
 Cladding = mp.Medium(index=1)
 # Cladding = SiO2  # comment for air
 
 t_si = 0.220
 t_ped = 0.050
-w_ped = 4
-t_box = 1.5
-t_top = 1.5  # air
+w_ped = 3
+t_box = 1.2
+t_top = 1.2  # air
 
 resolution = 64  # pixels/um
-sc_y = 5
+sc_y = 3.5
 sc_z = t_box + t_top
 geometry_lattice = mp.Lattice(size=mp.Vector3(0,sc_y,sc_z))
 
-num_bands = 2
-
-def get_geom(wg_width=0.35):
+def get_xs(wg_width=0.35, two_wg_gap=None, encapsulation=None):
     geometry = []
-    # # Cladding
+    # Cladding
     geometry.append(mp.Block(size=mp.Vector3(mp.inf, mp.inf, t_top),
                      center=mp.Vector3(z=t_top/2), material=Cladding))
     # BOX
     geometry.append(mp.Block(size=mp.Vector3(mp.inf, mp.inf, t_box),
                      center=mp.Vector3(z=-t_box/2), material=SiO2))
+    if encapsulation is not None:
+        t_ped_encap = t_ped + encapsulation
+        t_rib_encap = t_si + encapsulation
+        geometry.append(mp.Block(size=mp.Vector3(mp.inf, mp.inf, t_ped_encap),
+                        center=mp.Vector3(z=t_ped_encap/2), material=SiO2))
+        geometry.append(mp.Block(size=mp.Vector3(mp.inf, wg_width + 2*encapsulation, t_rib_encap),
+                        center=mp.Vector3(z=t_rib_encap/2), material=SiO2))
     # partial
     geometry.append(mp.Block(size=mp.Vector3(mp.inf, w_ped, t_ped),
-                     center=mp.Vector3(z=t_ped/2), material=Si))
+                     center=mp.Vector3(z=t_ped/2), material=ColdSi))
     # core
-    geometry.append(mp.Block(size=mp.Vector3(mp.inf, wg_width, t_si),
-                    center=mp.Vector3(z=t_si/2), material=Si))
+    if two_wg_gap is None:
+        offsets = [0]
+    else:
+        offsets = np.array([-1, 1]) * (wg_width + two_wg_gap)/2
+    for offset in offsets:
+        geometry.append(mp.Block(size=mp.Vector3(mp.inf, wg_width, t_si),
+                        center=mp.Vector3(y=offset, z=t_si/2), material=ColdSi))
     return geometry
 
 
-def get_ms(geom = None, k_points=[0,1]):
+def get_ms(geom = None, k_points=[0,1], num_bands=1):
     if geom is None:
-        geom = get_geom()
+        geom = get_xs()
     ms = mpb.ModeSolver(geometry_lattice=geometry_lattice,
                         geometry=geom,
                         k_points=k_points,
@@ -52,8 +63,10 @@ def cutoff_k(freq):
     return freq * np.sqrt(SiO2.epsilon_diag[0])
 
 
-def get_ks(wg_width=0.35, freq=1/1.218):
-    ms = get_ms(get_geom(wg_width))
+def get_ks(xs=None, freq=1/1.218, num_bands=1):
+    if xs is None:
+        xs = get_xs()
+    ms = get_ms(xs)
 
     band_min = 1
     band_max = num_bands
@@ -63,7 +76,7 @@ def get_ks(wg_width=0.35, freq=1/1.218):
     kmag_min = freq*0.1
     kmag_max = freq*4.0
 
-    k_calc = ms.find_k(mp.EVEN_Y,
+    k_calc = ms.find_k(mp.ODD_Y,
                        freq, band_min, band_max, kdir, tol,
                        kmag_guess, kmag_min, kmag_max,)
                        # mpb.output_poynting_x)#, mpb.display_group_velocities)
